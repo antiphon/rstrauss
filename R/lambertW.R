090d1def16e40d9904d9933b26aadeea2abf9159
Tuomas Rajala <tuomas.rajala@iki.fi>
2014-05-26 09:45:28 +0200

    Function for MGF intensity approximation from beta,gamma,range.

---
+++ R/lambertW.R
@@ -0,0 +1,15 @@
+#' Lambert's W function order 0
+#' 
+#' shamelessly copied from spatstat.
+#' 
+#' @export
+
+LambertW <- function (x) 
+{
+  if (require(gsl, quietly = TRUE)) 
+    return(gsl::lambert_W0(x))
+  result <- rep.int(NA_real_, length(x))
+  for (i in which(is.finite(x) & (x >= 0))) result[i] <- uniroot(yexpyminusx, 
+                                                                 c(0, x[i]), x = x[i])$root
+  return(result)
+}
--- R/theory.R
+++ R/theory.R
@@ -5,7 +5,7 @@
 #' 
 #' @export
 
-strauss_theory <- function(intensity=100, dim=3) {
+strauss_theory <- function(intensity, dim=3) {
   if(dim==3) {
     maxr <- 2*(3/(2*pi^2*intensity))^(1/3)
     c(max_range=maxr)
@@ -14,4 +14,15 @@ strauss_theory <- function(intensity=100, dim=3) {
     maxr <-2*(2/(pi^2*intensity))^(1/2)
     c(max_range=maxr)
   }
+}
+
+#' Approximate intensity for Strauss based on beta,gamma,range
+#' 
+#' Use Poisson mgf approximation.
+#' 
+#' @export
+
+strauss_intensity <- function(beta, gamma, range, dim=3){
+  G <- - log(gamma) * range^dim * ( if(dim==3) 3*pi/4 else pi )
+  LambertW(beta*G)/G
 }
