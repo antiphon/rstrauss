2f510bad9c9b79ed1f1a6bd9bc4b8073be6ef2f9
Tuomas Rajala <tuomas.rajala@iki.fi>
2014-07-30 16:41:20 +0200

    New constant approximation using Gaussian approximation.

--- R/approx_Z_master.R
+++ R/approx_Z_master.R
@@ -6,7 +6,7 @@
 #' @param gamma gamma
 #' @param range range
 #' @param bbox bounding box
-#' @param method OT, PS or P
+#' @param method OT, PS, P or R. Default: R
 #' @param ... further details for methods
 #' 
 #' Conditioning on number of points can be done with OT and P by setting the named parameter n.
@@ -19,13 +19,15 @@
 #' 
 #' PS: MC unbiased estimate, Path sampling Berthelsen&Moller 2003. \code{\link{approximate_strauss_constant_PS}}
 #' 
+#' R: My own version for unconditional Z. Based on a normal approximation (not published). Works when beta*Area >> 0
+#' 
 #' @export
 
-approximate_strauss_constant <- function(beta, gamma, range, bbox, method="P", ...) {
+approximate_strauss_constant <- function(beta, gamma, range, bbox, method="R", ...) {
   if(missing(beta)|missing(gamma)|missing(range))stop("Provide beta, gamma and range.")
   if(missing(bbox)) stop("Provide bounding box, column-wise ranges matrix.")
-  methods <- c("OT", "P", "PS")
-  fs <- sapply(paste0("approximate_strauss_constant_", c("OT", "penttinen", "PS") ), get)
+  methods <- c("OT", "P", "PS", "R")
+  fs <- sapply(paste0("approximate_strauss_constant_", c("OT", "penttinen", "PS", "R") ), get)
   i <- which(match.arg(method, methods)==methods)
   fs[[i]](beta,gamma, range, bbox, ...)
 }
